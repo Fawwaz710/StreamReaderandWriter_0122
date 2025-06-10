b8be72fbbf51ba3519cd978b0fbbd0304c1e9af3
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-06-10 18:48:11 +0700

    membuat manual exception

--- manualexception.cpp
+++ manualexception.cpp
@@ -5,12 +5,18 @@ int main() {
     try
     {
         cout << "Selamat belajar di prodi TI UMY" << endl;
-        throw 0.5; //
+        throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi " << endl;
     }
     catch(int a)
+    //blok ini akan dieksekusi
     {
         cout << "Pengecualian akan dieksekusi " << endl;
     }
+    catch (...) {
+        //jika selain integer maka blok ini akan dieksekusi
+        cout << "default pengecualian dieksekusi " << endl;
+    }
+    return 0;
     
 }
