b97e7a16fe85e4eb21ddbd4a28b902d44630af68
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-06-10 17:27:23 +0700

    memberikan penjelasan

--- ioFile.cpp
+++ ioFile.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main() {
     string baris;
+    // membuka file dalam mode menulis 
     ofstream outfile;
+    // menunjuk ke sebuah nama file 
     outfile.open("contohfile.txt");
-    cout << ">= Menulis file, \'q\ untuk keluar" << endl;
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    // unlimited loop untuk menulis 
     while (true)
     {
         cout << "-";
